b775bca13440aee3c8bf1a06c36cf972057b8e40
marcoshakuro <77284884+marcoshakuro@users.noreply.github.com>
2021-03-02 09:40:24 -0300

    camada de security implementada em blog pessoal

--- SpringBoot/blogPessoal/src/main/java/com/generation/blogPessoal/model/Postagem.java
+++ SpringBoot/blogPessoal/src/main/java/com/generation/blogPessoal/model/Postagem.java
@@ -6,31 +6,41 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "postagens")
 public class Postagem {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
-	
+
 	@NotNull
 	@Size(min = 5, max = 100)
 	private String titulo;
-	
+
 	@NotNull
 	@Size(min = 5, max = 500)
 	private String texto;
-	
+
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagens")
+	private Tema tema;
+
+	public Tema getTema() {
+		return tema;
+	}
 
 	public long getId() {
 		return id;
@@ -64,5 +74,8 @@ public class Postagem {
 		this.date = date;
 	}
 
-	
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
 }
---
+++ SpringBoot/blogPessoal/src/main/java/com/generation/blogPessoal/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.generation.blogPessoal.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.generation.blogPessoal.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, String>{
+	public Optional<Usuario> findByUsuario(String usuario);
+}
