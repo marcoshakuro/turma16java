9f487958dbee1c54e979929e79e2a32ee817f2ec
marcoshakuro <77284884+marcoshakuro@users.noreply.github.com>
2021-03-03 11:39:34 -0300

    atualização de segurança

--- SpringBoot/blogPessoal/src/main/java/com/generation/blogPessoal/controller/UsuarioController.java
+++ SpringBoot/blogPessoal/src/main/java/com/generation/blogPessoal/controller/UsuarioController.java
@@ -31,7 +31,12 @@ public class UsuarioController {
 
 	@PostMapping("/cadastrar")
 	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario){
-		return ResponseEntity.status(HttpStatus.CREATED)
-				.body(usuarioService.CadastrarUsuario(usuario));
+		Optional<Usuario> user = Optional.ofNullable(usuarioService.CadastrarUsuario(usuario));
+		try {
+				return ResponseEntity.ok(user.get());
+		}catch (Exception e) {
+			
+				return ResponseEntity.badRequest().build();
+		}
 	}
 }
